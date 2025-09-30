060122b2c830bd9cc425180ce3af15c2e8c31a2f
1LuKz <lucasedu292003@gmail.com>
2025-09-29 22:56:47 -0300

    User and controller

---
+++ src/main/java/com/lucasstore/store/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.lucasstore.store.controllers;
+
+import com.lucasstore.store.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User myUser = new User(1, "Lucas", "lucas@email.com", "31988335566", "myPassword123");
+        return ResponseEntity.status(HttpStatus.OK).body(myUser);
+    }
+}
