4def631d8feac747e0f22cab81f0479cf993efdb
1LuKz <lucasedu292003@gmail.com>
2025-09-29 23:36:40 -0300

    H2 database and test profile

--- src/main/java/com/lucasstore/store/models/User.java
+++ src/main/java/com/lucasstore/store/models/User.java
@@ -1,13 +1,11 @@
 package com.lucasstore.store.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
