1a1cafafaa8abc57b7fda492c163f33a853879f2
1LuKz <lucasedu292003@gmail.com>
2025-09-30 23:43:29 -0300

    Service layer

--- src/main/java/com/lucasstore/store/controllers/UserController.java
+++ src/main/java/com/lucasstore/store/controllers/UserController.java
@@ -1,21 +1,34 @@
 package com.lucasstore.store.controllers;
 
 import com.lucasstore.store.models.User;
-import com.lucasstore.store.repositories.UserRepository;
+import com.lucasstore.store.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User myUser = new User(1, "Lucas", "lucas@email.com", "31988335566", "myPassword123");
-        return ResponseEntity.status(HttpStatus.OK).body(myUser);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        User user = userService.findById(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(user);
     }
 }
